1c311bb0c4c20359974f7dd73133320f646eff5e
latticetower <merlettaia@gmail.com>
2020-04-02 07:02:53 +0300

    add dummy predictors

--- predictors/basic.py
+++ predictors/basic.py
@@ -1,11 +1,15 @@
+import numpy as np
+
 from base.iodata import IOData
 from base.field import Field
+
 
 class Predictor:
     def train(self, iodata):
         pass
     def predict(self, field):
         pass
+
 
 class IdPredictor(Predictor):
     def train(self, iodata_list):
@@ -14,3 +18,24 @@ class IdPredictor(Predictor):
         if isinstance(field, IOData):
             return self.predict(field.input_field)
         return Field(field.data)
+
+
+class ZerosPredictor(Predictor):
+
+    def train(self, iodata_list):
+        pass
+
+    def predict(self, field):
+        if isinstance(field, IOData):
+            return self.predict(field.input_field)
+        return Field(np.zeros((field.height, field.width)))
+
+class OnesPredictor(Predictor):
+
+    def train(self, iodata_list):
+        pass
+
+    def predict(self, field):
+        if isinstance(field, IOData):
+            return self.predict(field.input_field)
+        return Field(np.ones((field.height, field.width)))
