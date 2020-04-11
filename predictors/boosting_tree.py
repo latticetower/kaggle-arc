038bd28101a4bf8c350aeaaa9447cfbde581e4b7
latticetower <merlettaia@gmail.com>
2020-04-11 17:23:18 +0300

    add features

--- predictors/boosting_tree.py
+++ predictors/boosting_tree.py
@@ -2,6 +2,7 @@
 Based on https://www.kaggle.com/meaninglesslives/using-decision-trees-for-arc
 """
 from xgboost import XGBClassifier
+from itertools import product
 
 from base.field import Field
 from base.iodata import IOData
@@ -9,41 +10,41 @@ from base.iodata import IOData
 from predictors.basic import *
 
 
-def get_moore_neighbours(field, cur_row, cur_col, nrows, ncols):
+def get_moore_neighbours(field, cur_row, cur_col, nrows, ncols, color=0):
     if cur_row <= 0:
-        top = -1
+        top = color
     else:
         top = field.data[cur_row - 1, cur_col]
         
     if cur_row >= nrows - 1:
-        bottom = -1
+        bottom = color
     else:
         bottom = field.data[cur_row + 1, cur_col]
         
     if cur_col <= 0:
-        left = -1
+        left = color
     else:
         left = field.data[cur_row, cur_col - 1]
         
     if cur_col >= ncols - 1:
-        right = -1
+        right = color
     else:
         right = field.data[cur_row, cur_col + 1]
         
     return top, bottom, left, right
 
 
-def get_tl_tr(field, cur_row, cur_col, nrows, ncols):
+def get_tl_tr(field, cur_row, cur_col, nrows, ncols, color=0):
     if cur_row == 0:
-        top_left = -1
-        top_right = -1
+        top_left = color
+        top_right = color
     else:
         if cur_col == 0:
-            top_left =- 1
+            top_left = color
         else:
             top_left = field.data[cur_row - 1, cur_col - 1]
         if cur_col == ncols - 1:
-            top_right =- 1
+            top_right = color
         else:
             top_right = field.data[cur_row - 1, cur_col + 1]   
         
@@ -52,23 +53,50 @@ def get_tl_tr(field, cur_row, cur_col, nrows, ncols):
 
 def make_features(field, nfeat=13, local_neighb=5):
     nrows, ncols = field.shape
-    feat = np.zeros((nrows*ncols, nfeat))
+    #feat = np.zeros((nrows*ncols, nfeat))
+    all_features = []
     cur_idx = 0
     for i in range(nrows):
         for j in range(ncols):
-            feat[cur_idx,0] = i
-            feat[cur_idx,1] = j
-            feat[cur_idx,2] = field.data[i, j]
-            feat[cur_idx,3:7] = get_moore_neighbours(field, i, j, nrows, ncols)
-            feat[cur_idx,7:9] = get_tl_tr(field, i, j, nrows, ncols)
-            feat[cur_idx,9] = len(np.unique(field.data[i,:]))
-            feat[cur_idx,10] = len(np.unique(field.data[:,j]))
-            feat[cur_idx,11] = (i+j)
-            feat[cur_idx,12] = len(np.unique(field.data[i-local_neighb:i+local_neighb,
-                                                         j-local_neighb:j+local_neighb]))
+            features = [
+                i,
+                j,
+                field.data[i, j]]
+            features.extend(get_moore_neighbours(field, i, j, nrows, ncols))
+            features.extend(get_tl_tr(field, i, j, nrows, ncols))
+            features.extend([
+                len(np.unique(field.data[i,:])),
+                len(np.unique(field.data[:,j])),
+                #next goes count of non-zero points
+                np.sum(field.data[i, :] > 0),
+                np.sum(field.data[:, j] > 0),
+                (i+j),
+                len(np.unique(field.data[
+                    i-local_neighb:i+local_neighb,
+                    j-local_neighb:j+local_neighb]))
+            ])
+            
+            #feat[cur_idx,13]
+            features.extend([
+                (i + ncols - j - 1),
+                (i + j) % 2,
+                (i + j + 1) % 2,
+                #(i + ncols - j - 1) % 2
+                #(nrows - 1 - i + ncols - j - 1),
+                #(nrows - 1 - i + j)
+            ])
+            features.extend([
+                field.get(i + k, j + v)
+                for k, v in product([-1, 0, 1], [-1, 0, 1])
+            ])
+            features.extend([
+                field.data[nrows - 1 - i, j],
+                field.data[nrows - 1 - i, ncols - 1 - j],
+                field.data[i, ncols - 1 - j]
+            ])
+            all_features.append(features)
 
-            cur_idx += 1
-        
+    feat = np.asarray(all_features)
     return feat
 
 def get_features(iodata_list):
@@ -94,7 +122,7 @@ def get_features(iodata_list):
 
 class BoostingTreePredictor(Predictor, AvailableEqualShape):
     def __init__(self):
-        self.xgb =  XGBClassifier(n_estimators=25, n_jobs=-1)
+        self.xgb =  XGBClassifier(n_estimators=25, booster="dart", n_jobs=-1)
 
     def train(self, iodata_list):
         feat, target, _ = get_features(iodata_list)
