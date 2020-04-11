fcce606516e2f5a3feb485f48822035151bf75ba
latticetower <merlettaia@gmail.com>
2020-04-11 19:07:17 +0300

    add mirroring

--- operations/resizing.py
+++ operations/resizing.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from operations.basic import Operation
 
 
@@ -15,6 +17,31 @@ class Repeater(Operation):
         for i in range(height):
             for j in range(width):
                 result[i::height, j::width] = data[i, j]
+        return result
+
+
+class Mirror(Operation):
+    def __init__(self, m1, m2, horizontal=True, vertical=True):
+        self.m1 = m1
+        self.m2 = m2
+        self.horizontal = horizontal
+        self.vertical = vertical
+
+    def __call__(self, data):
+        height, width = data.shape
+        result = np.zeros((height * self.m1, width * self.m2),
+            dtype=data.dtype)
+        #for offset1 in range(self.m1):
+        #    for offset2 in range(self.m2):
+        for i in range(height):
+            for j in range(width):
+                result[i::height, j::width] = data[i, j]
+                if self.vertical:
+                    result[height + i:: 2 * height, j::width] = data[height - 1 - i, j]
+                if self.horizontal:
+                    result[i:: height, width + j :: 2 * width] = data[i, width - 1 - j]
+                if self.horizontal and self.vertical:
+                    result[height + i :: 2 * height, width + j :: 2 * width] = data[height - 1 - i, width - 1 - j]
         return result
 
 
