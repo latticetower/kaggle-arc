f33028d552c6531dad9c8bda803aae3ce4b37c8f
Tanya Malygina <merlettaia@gmail.com>
2024-06-17 02:57:01 +0200

    fix

--- kaggle_arc/utils.py
+++ kaggle_arc/utils.py
@@ -1,5 +1,5 @@
 import rootutils
-root = rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+root = rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True, cwd=True)
 
 import json
 import os
